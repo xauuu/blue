06c10ea31be06b6748bea150889360adefb3b473
xauuu <qdatqb@gmail.com>
2020-12-06 10:34:43 +0700

    no

--- resources/views/pages/products/product-detail.blade.php
+++ resources/views/pages/products/product-detail.blade.php
@@ -56,36 +56,6 @@
                         @endfor
                         <li class="rat">{{ count($detail->rating) }} đánh giá</li>
                     </ul>
-                    <div class="p-review">
-                        <a>{{ count($detail->comment) }} bình luận</a>|<a href="#cmt">Thêm bình luận của bạn</a>
-                    </div>
-                    <div class="fw-size-choose">
-                        <p>Size</p>
-                        <div class="sc-item">
-                            <input type="radio" name="sc" id="xs-size">
-                            <label for="xs-size">32</label>
-                        </div>
-                        <div class="sc-item">
-                            <input type="radio" name="sc" id="s-size">
-                            <label for="s-size">34</label>
-                        </div>
-                        <div class="sc-item">
-                            <input type="radio" name="sc" id="m-size" checked="">
-                            <label for="m-size">36</label>
-                        </div>
-                        <div class="sc-item">
-                            <input type="radio" name="sc" id="l-size">
-                            <label for="l-size">38</label>
-                        </div>
-                        <div class="sc-item disable">
-                            <input type="radio" name="sc" id="xl-size" disabled>
-                            <label for="xl-size">40</label>
-                        </div>
-                        <div class="sc-item">
-                            <input type="radio" name="sc" id="xxl-size">
-                            <label for="xxl-size">42</label>
-                        </div>
-                    </div>
                     <div class="quantity">
                         <p>Số lượng</p>
                         <div class="pro-qty"><input name="quantity" type="text" value="1"></div>
